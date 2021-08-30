c35cad27ccb368d04975589429537aaad8a56c95
BunnyNomics101 <89548046+BunnyNomics101@users.noreply.github.com>
2021-08-30 18:02:38 +0200

    Delete PCB1.R

--- PCB1.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-df<-data.table::fread("pcb1.csv", sep = ";", dec=",",
-                      stringsAsFactors = FALSE)
-df <- df %>%
-  mutate(timestamp = as.Date(timestamp, "%d.%m.%y"))
-df$close <- log(as.numeric(df$close))
-pcb_plot <- ggplot(df, aes(x=timestamp, y= close, color=token)) + geom_point() + geom_smooth()
-print(pcb_plot + labs(y="log($Price)", x = "Time"))
-
